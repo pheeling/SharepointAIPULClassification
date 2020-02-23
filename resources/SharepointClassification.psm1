04f913e1b5b446a0f3f1368b74337fa94dfd8bcc
pheeling <philipph@vitalitystream.ch>
2020-02-23 13:55:42 +0100

    Add Filter Classification only for not labeled files

--- resources/SharepointClassification.psm1
+++ resources/SharepointClassification.psm1
@@ -36,7 +36,7 @@ class SharepointClassification {
         
                 Write-host $element
                 "$(Get-Date) [SharepointClassification] File for Classification $element : $($item.Fieldvalues.FileRef)" >> $Global:logFile
-                $element | Set-AIPFileLabel -LabelId $labelId -Owner $dataOwner -PreserveFileDetails | Export-Csv -Append "$Global:resourcespath\AIPResultStatus.csv"
+                $element | Get-AIPFileStatus | Where-Object {$_.IsLabeled -eq $false} | Set-AIPFileLabel -LabelId $labelId -Owner $dataOwner -PreserveFileDetails | Export-Csv -Append "$Global:resourcespath\AIPResultStatus.csv"
                 $element | Get-AIPFileStatus | Export-csv -Append "$Global:resourcespath\AIPFilestatus.csv"
                 Add-PnPFile -Path "$($element)" -Folder $item.FieldValues.FileDirRef.Substring($sharepointLoginUrl.AbsolutePath.Length) -Checkout
                 Remove-Item -Path "$($element)" -Confirm:$false -Force    
