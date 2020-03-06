1344cd9d11f8faf2f00802f03836dcf4df12e87a
Philipp Hiestand (Admin) <phadmin@dinotronic.ch>
2020-03-06 23:13:26 +0100

    corrected Alpha und Beta Issues

--- resources/SharepointClassification.psm1
+++ resources/SharepointClassification.psm1
@@ -9,7 +9,7 @@ class SharepointClassification {
     [PSCredential] $sharepointCredentials
     [String] $svcCredentialFilepath = "$Global:resourcespath\${env:USERNAME}_svcCred_$($tenantID).xml"
     [PSCredential] $svcCredentials
-    [Array] $documentsList
+    [Array] $documentsList = @()
 
     SharepointClassification($tenantID){
         if (!(Test-Path $this.sharepointCredentialFilepath)) {
@@ -21,7 +21,7 @@ class SharepointClassification {
 
     connectSPO([uri] $sharepointLoginURL){
         #Connect-PnPOnline -url  $sharepointLoginUrl -credential $this.sharepointCredentials
-        Connect-PnPOnline -url  $sharepointLoginUrl -UseWebLogin
+        Connect-PnPOnline -url $sharepointLoginUrl -UseWebLogin
     }
 
     connectAIPService([String] $webAppID, [String] $webAppKey){
@@ -41,22 +41,24 @@ class SharepointClassification {
 
     getSharepointLibraryEntries($array){
         foreach ($entry in $array){
-            $this.documentsList += Get-PnPFolder -List $entry
+            $this.documentsList += Get-PnPListItem -List $entry -PageSize 100
         }
     }
 
     fileClassification($documentsList, $labelId, $dataOwner, $sharepointLoginUrl){
-        foreach ($item in $documentsList) { 
+        foreach ($item in $documentsList) {
             if (($item.Fieldvalues.FileRef -like "*.docx") -or ($item.Fieldvalues.FileRef -like "*.xlsx") -or ($item.Fieldvalues.FileRef -like "*.pptx")) { 
                 Get-PnPFile -Url $item.FieldValues.FileRef -Path $Global:resourcespath -AsFile -Force
                 $element = "$($Global:resourcespath)\$($item.Fieldvalues.FileLeafRef)"
+                $modifiedBy = Get-PnPUser | Where-Object {$_.Title -eq $item.FieldValues.Editor.LookupValue}
+                $modifiedDate = $($item.FieldValues.Modified)
         
                 Write-host $element
                 "$(Get-Date) [SharepointClassification] File for Classification $element : $($item.Fieldvalues.FileRef)" >> $Global:logFile
                 $element | Get-AIPFileStatus | Where-Object {$_.IsLabeled -eq $false} | Set-AIPFileLabel -LabelId $labelId -Owner $dataOwner -PreserveFileDetails | Export-Csv -Append "$Global:resourcespath\AIPResultStatus.csv"
                 $element | Get-AIPFileStatus | Export-csv -Append "$Global:resourcespath\AIPFilestatus.csv"
-                Add-PnPFile -Path "$($element)" -Folder $item.FieldValues.FileDirRef.Substring($sharepointLoginUrl.AbsolutePath.Length) -Checkout
-                Remove-Item -Path "$($element)" -Confirm:$false -Force    
+                Add-PnPFile -Path "$($element)" -Folder $item.FieldValues.FileDirRef.Substring($sharepointLoginUrl.AbsolutePath.Length) -Values @{Editor=$modifiedBy.Email;Modified=$modifiedDate} -Checkout
+                Remove-Item -Path "$($element)" -Confirm:$false -Force
             }
         }
     }
