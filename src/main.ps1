acbf5d31e9b57fc8eff657f18d7e5d8e194c791f
Philipp. Hiestand <33524582+pheeling@users.noreply.github.com>
2021-01-11 00:34:32 +0100

    New filter possibilities

--- src/main.ps1
+++ src/main.ps1
@@ -11,7 +11,10 @@ Param(
     [Parameter(Mandatory=$true, HelpMessage = "Define Path to Sharepoint Libraries List Text File?")]
     [String]$documentLibraryList,
 
-    [Parameter(Mandatory=$true, HelpMessage = "Define WebApp ID for connection?")]
+    [Parameter(Mandatory=$true, HelpMessage = "Define Path to Sharepoint Libraries List Text File?")]
+    [String]$filterFileList,
+
+    [Parameter(Mandatory=$false, HelpMessage = "Define WebApp ID for connection?")]
     [String]$webAppID,
     
     [Parameter(Mandatory=$true, HelpMessage = "Sharepoint URL?")]
@@ -23,7 +26,7 @@ $Global:mainPath = split-path -path $srcPath
 $Global:resourcespath = join-path -path "$mainPath" -ChildPath "resources"
 $Global:errorVariable = "Stop"
 $Global:logFile = "$resourcespath\processing.log"
-$webAppKeyXMLFile = "$Global:resourcespath\${env:USERNAME}_appKey_$($tenantID).xml"
+#$webAppKeyXMLFile = "$Global:resourcespath\${env:USERNAME}_appKey_$($tenantID).xml"
 
 Import-Module -Force "$resourcespath\ErrorHandling.psm1"
 Import-Module -Force "$resourcespath\SharepointClassification.psm1"
@@ -34,32 +37,43 @@ $sharepoint = Get-NewSharepointClassification($tenantID)
 
 #Requirements Check
 try {
-    if ((Get-InstalledModule -name  Microsoft.Online.Sharepoint.Powershell | 
-        Select-Object -ExpandProperty version) -ge 16.0.19724.12000) {
+    if (Get-Command Connect-PnPOnline) {
         "$(Get-Date) [RequirementsCheck] Module Sharepoint exists" >> $Global:logFile
+    } else {
+        Install-Module SharePointPnPPowerShellOnline
     }
-    else {
-        Install-Module -name Microsoft.Online.Sharepoint.Powershell -AllowClobber -Force
+    if(Get-Command Connect-AipService -ErrorAction SilentlyContinue){
+        "$(Get-Date) [RequirementsCheck] Module AIP Classic exists" >> $Global:logFile
+    } else {
+        #Dependent on Docker Image
+        Import-Module "C:\source\AIP.dll"
+    }
+    if(Get-Command Connect-IPPSSession -ErrorAction SilentlyContinue){
+        "$(Get-Date) [RequirementsCheck] Module ExchangeOnlineManagement exists" >> $Global:logFile
+    } else {
+        #Dependent on Docker Image
+        Install-Module ExchangeOnlineManagement
     }
     if(Get-Command Get-AIPFileStatus -ErrorAction SilentlyContinue){
         "$(Get-Date) [RequirementsCheck] Module AIP exists" >> $Global:logFile
-    }
-    if(!(Test-Path $webAppKeyXMLFile)){
-        Get-Credential | Export-Clixml -Path $webAppKeyXMLFile
     } else {
-        "$(Get-Date) [RequirementsCheck] token exists" >> $Global:logFile
-        $webAppKeyXML = Import-Clixml $webAppKeyXMLFile
-        $sharepoint.connectAIPService($webAppID,$webAppKeyXML.GetNetworkCredential().Password)
+        #Dependent on Docker Image
+        Import-Module "C:\source\AIP.dll"
     }
+
+    
 } catch {
     "$(Get-Date) [RequirementsCheck] Module installation failed: $PSItem" >> $Global:logFile
     #Get-NewErrorHandling "$(Get-Date) [RequirementsCheck] Module installation failed" $PSItem
 }
 
+$sharepoint.connectAIPServiceClassic()
+$sharepoint.connectAIPServiceUL()
 $sharepoint.connectSPO($sharepointLoginUrl)
-$arrayLibrary = $sharepoint.readDocumentLibraryListFile($documentLibraryList)
+$arrayLibrary = $sharepoint.readTextInputs($documentLibraryList)
+$arrayFilter = $sharepoint.readTextInputs($filterFileList)
 $sharepoint.getSharepointLibraryEntries($arrayLibrary)
-$sharepoint.fileClassification($sharepoint.documentsList, $labelId, $dataOwner, $sharepointLoginUrl)
+$sharepoint.fileClassification($sharepoint.documentsList, $labelId, $dataOwner, $sharepointLoginUrl, $arrayFilter)
 
 "$(Get-Date) [Processing] Stopped -----------------------" >> $Global:logFile
 
